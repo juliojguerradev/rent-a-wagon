2180f083edafe2207e6d80d16fc19260a987286f
Julio Guerra <juliowar33@gmail.com>
2021-11-13 15:44:26 -0500

    Add vehicle migration

--- db/migrate/20211113191132_create_vehicles.rb
+++ db/migrate/20211113191132_create_vehicles.rb
@@ -1,8 +1,9 @@
 class CreateVehicles < ActiveRecord::Migration[6.0]
   def change
     create_table :vehicles do |t|
+      t.string :name
       t.text :description
-      t.text :model
+      t.string :model
       t.float :rent_cost_per_day
 
       t.timestamps
