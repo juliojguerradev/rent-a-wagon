b670b1acd3a853ce5abb4df3b1163e506f8501bb
Julio Guerra <juliowar33@gmail.com>
2021-11-16 18:39:02 -0500

    Add brand field

--- db/migrate/20211113191132_create_vehicles.rb
+++ db/migrate/20211113191132_create_vehicles.rb
@@ -4,6 +4,7 @@ class CreateVehicles < ActiveRecord::Migration[6.0]
       t.string :name
       t.text :description
       t.string :model
+      t.string :brand
       t.float :rent_cost_per_day
 
       t.timestamps
