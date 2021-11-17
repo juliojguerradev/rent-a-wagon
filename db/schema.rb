f67cf4b6b830b212ad399d4972b9142bd796cdf7
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2021-11-16 22:27:44 -0300

    migration vehicles table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_13_190610) do
+ActiveRecord::Schema.define(version: 2021_11_13_191132) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,16 @@ ActiveRecord::Schema.define(version: 2021_11_13_190610) do
     t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "vehicles", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "model"
+    t.string "brand"
+    t.float "rent_cost_per_day"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
