b6c5e7422675df69cb79f7d9391936409bda5ea9
Eduardo Cifuentes <edu.cifuentes@gmail.com>
2021-11-20 13:45:14 -0300

    fake address to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@ User.new(email: "julio@lewagon.com", password: "123456").save
 
 User.all.each do |user|
   2.times do
-    car = Vehicle.new(name: Faker::Vehicle.make_and_model, description: "a good car", model: Faker::Vehicle.model, brand: Faker::Vehicle.make, rent_cost_per_day: (10..20).to_a.sample)
+    car = Vehicle.new(name: Faker::Vehicle.make_and_model, description: "a good car", model: Faker::Vehicle.model, brand: Faker::Vehicle.make, rent_cost_per_day: (10..20).to_a.sample, address: Faker::Address.street_address)
     car.user = user
     car.save
   end
