2d67c37fcb8df8e326f03721c26b3e3e505ff937
fernandoac02 <66658826+fernandoac02@users.noreply.github.com>
2021-11-20 09:17:06 -0600

    Revert "Booking model"

--- app/models/booking.rb
+++
@@ -1,9 +0,0 @@
-class Booking < ApplicationRecord
-  belongs_to :vehicle
-  belongs_to :user
-
-  validates :start_date, presence: true
-  validates :end_date, presence: true
-  validates_associated :user
-  validates_associated :vehicle
-end
