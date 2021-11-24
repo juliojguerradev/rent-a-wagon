5d5b7597eca19c0ecac54bd9928c10fe0e4dace0
Julio Guerra <juliowar33@gmail.com>
2021-11-23 21:46:33 -0500

    Add address autocomplete when creating a new vehicle

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -28,6 +28,7 @@ import "bootstrap";
 import 'mapbox-gl/dist/mapbox-gl.css';
 // internal imports
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete'
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -36,4 +37,5 @@ document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
   initMapbox();
+  initAutocomplete();
 });
